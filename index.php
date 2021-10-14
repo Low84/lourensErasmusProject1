8a9edd3f7cde860577035673b32b1035af99284f
Low84 <63657914+Low84@users.noreply.github.com>
2021-10-14 13:35:34 +0100

    Commented out a test script for the select file

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
-    <script>
+    <!-- <script>
         $(document).ready(function () {
             let list_of_countries 
             $.ajax({
@@ -76,7 +76,7 @@
                 }
             });
         });
-    </script>
+    </script> -->
     <script src="./js/leaflet.js"></script>
     <script src="./js/script.js"></script>
 </body>
