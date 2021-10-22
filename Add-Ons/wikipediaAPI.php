40c1b884c1bb0531ff96ab62e596dab29be92a5b
Low84 <63657914+Low84@users.noreply.github.com>
2021-10-22 16:46:45 +0100

    moves to selected country

--- Add-Ons/wikipediaAPI.php
+++ Add-Ons/wikipediaAPI.php
@@ -29,3 +29,32 @@
 	echo json_encode($output); 
 
 ?>
+
+<?php
+
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+	$country = $_GET['country'];
+
+	$executionStartTime = microtime(true);
+	$url='https://api.opencagedata.com/geocode/v1/json?q='. $country .'&username=lo84';
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+
+	$decode = json_decode($result, true);
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['data'] = $decode['geonames'];
+
+	echo json_encode($output); 
+
+?>
--- php/latAndLongAPI.php
+++ php/latAndLongAPI.php
@@ -1,13 +1,11 @@
-<!-- API  https://opencagedata.com/api#quickstart forward geocoding example to get lat and long values from select list -->
-<!-- . $_REQUEST['country'] . ' -->
 <?php
 
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
+	$country = $_GET['country'];
 
 	$executionStartTime = microtime(true);
-
-	$url='https://api.opencagedata.com/geocode/v1/json?q=Albania&key=e629dbaaefe94793afde8b17b2947ad8';
+	$url='https://api.opencagedata.com/geocode/v1/json?q='. $country .'&key=e629dbaaefe94793afde8b17b2947ad8';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -18,16 +16,13 @@
 
 	curl_close($ch);
 
-	$decode = json_decode($result,true);	
+	$decode = json_decode($result, true);
+
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
-	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['results'][0]['annotations']['DMS'];
-	
-	header('Content-Type: application/json; charset=UTF-8');
+	$output['data'] = $decode['results'][0]['geometry'];
 
 	echo json_encode($output); 
-  
 
 ?>
