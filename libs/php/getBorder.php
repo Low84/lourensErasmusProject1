5bb42eb8e849f2fef5d76137a30ac84d2ca54ef6
Lourens Erasmus <63657914+Low84@users.noreply.github.com>
2021-11-14 12:35:04 +0000

    Update getBorder.php

--- libs/php/getBorder.php
+++ libs/php/getBorder.php
@@ -1,16 +1,17 @@
 <?php
 
 $string = file_get_contents("../data/countryBorders.geo.json");
-$json = json_decode($string);
-$geometry = $json->features;
+$json = json_decode($string, true);
+$features = $json['features'];
+// echo '<pre>' . print_r($json, true) . '</pre>';
 
-$country_code = $_GET['country_code'];
+$country_code = $_GET['countryCode'];
 
 $output_geom = "";
-for($i=0;$i<sizeof($geometry);$i++){
-    $geometry = $geometry[$i];
-    if($geometry["properties"]["iso_a2"] == $country_code){
-        $output_geom = $geometry->geometry;
+for($i=0;$i<sizeof($features);$i++){
+    $feature = $features[$i];
+    if($feature["properties"]["iso_a2"] == $country_code){
+        $output_geom = $feature['geometry']['coordinates'];
     }
 }
 echo(json_encode($output_geom));
