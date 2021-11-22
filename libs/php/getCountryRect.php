407135f1e38da55e87df5ccf738a411abbc0575d
Lourens Erasmus <63657914+Low84@users.noreply.github.com>
2021-11-22 15:43:41 +0000

    Add files via upload

---
+++ libs/php/getCountryRect.php
@@ -0,0 +1,38 @@
+<?php
+ 
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+    $country = $_GET['country'];
+ 
+    $executionStartTime = microtime(true);
+ 
+    $url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=en&country=' . $country . '&username=lo84';
+ 
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL,$url);
+ 
+    $result=curl_exec($ch);
+ 
+    curl_close($ch);
+ 
+    $decode = json_decode($result, true);   
+ 
+    $output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['data']['east'] = $decode['geonames'][0]['east'];
+    $output['data']['west'] = $decode['geonames'][0]['west'];
+    $output['data']['north'] = $decode['geonames'][0]['north'];
+    $output['data']['south'] = $decode['geonames'][0]['south'];
+    $output['data']['currencyCode'] = $decode['geonames'][0]['currencyCode'];
+    $output['data']['countCode'] = $decode['geonames'][0]['countryCode'];
+    $output['data']['population'] = $decode['geonames'][0]['population'];
+
+
+ 
+  
+    echo json_encode($output); 
+ 
+?>
