34431cc93d4f408a93cdb07e6aeabc7e6bc0d098
Lourens Erasmus <63657914+Low84@users.noreply.github.com>
2021-11-22 15:43:04 +0000

    Update exchangeAPI.php

--- libs/php/exchangeAPI.php
+++ libs/php/exchangeAPI.php
@@ -2,11 +2,10 @@
 
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
-  $currencyCode = $_GET['currencyCode'];
 
 	$executionStartTime = microtime(true);
 
-	$url='https://v6.exchangerate-api.com/v6/93790965e545e00b206ee6e8/latest/' . $currencyCode;
+	$url='https://v6.exchangerate-api.com/v6/93790965e545e00b206ee6e8/latest/' . $_REQUEST['currencyCode'];
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -23,8 +22,8 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['data']['exchangeUsd'] = $decode['conversion_rates']['USD'];
-  $output['data']['exchangeGbp'] = $decode['conversion_rates']['GBP'];
-  $output['data']['exchangeEur'] = $decode['conversion_rates']['EUR'];
+  	$output['data']['exchangeGbp'] = $decode['conversion_rates']['GBP'];
+  	$output['data']['exchangeEur'] = $decode['conversion_rates']['EUR'];
 
 
 	echo json_encode($output); 
